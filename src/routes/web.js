2a6aee56d65ee851d9d4d54c662d670e52762666
Colinz <cuongbzo123@gmail.com>
2025-10-01 23:35:38 +0700

    first commit Colinz

---
+++ src/routes/web.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const { getHomepage, getABC, getHoiDanIT } = require('../controllers/homeController')
+const router = express.Router()
+
+router.get('/', getHomepage)
+
+router.get('/abc', getABC)
+
+
+router.get('/hoidanit', getHoiDanIT)
+
+module.exports = router
